702c7c360824699c8466d5e327400b29ca29ba96
fontain richardson <fontainwr@gmail.com>
2023-07-09 17:42:18 +0900

    removed some texts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # Add more stuff
   # Defines the root path route ("/")
   root "posts#index"
 end
